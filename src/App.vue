0247ff4479fc48943f1911cb6a2fb975d4b7f9cc
Timur Vakhitov <tukerty@gmail.com>
2018-08-05 16:31:54 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Panel/>
+  </div>
+</template>
+
+<script>
+import Panel from './components/Panel'
+
+export default {
+  name: 'App',
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
